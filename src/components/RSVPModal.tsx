c2bad65cb7caa557b15c570590915c976570e217
rajpatel923 <rajpatel123370@gmail.com>
2026-06-21 18:08:20 -0500

    Update contact information in RSVP modal for consistency

--- src/components/RSVPModal.tsx
+++ src/components/RSVPModal.tsx
@@ -216,8 +216,8 @@ export default function RSVPModal({ onClose }: Props) {
                             </svg>
                         </span>
                         <span>
-                            <span style={{ color: '#8c6d4b', fontSize: '0.7rem', marginRight: '0.3rem' }}>Umangini</span>
-                            361-944-8671
+                            <span style={{ color: '#8c6d4b', fontSize: '0.7rem', marginRight: '0.3rem' }}>Jayendra Patel</span>
+                            361-944-8771
                         </span>
                     </a>
                 </div>
@@ -275,7 +275,7 @@ export default function RSVPModal({ onClose }: Props) {
                             lineHeight: 1.5,
                         }}>
                             We already have your response on file.
-                            <br />Need to make a change? Call or text Umangini.
+                            <br />Need to make a change? Call or text Jayendra Patel.
                         </p>
                         <a
                             href="tel:+13619448671"
@@ -298,7 +298,7 @@ export default function RSVPModal({ onClose }: Props) {
                             <svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="#365744" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07A19.5 19.5 0 013.07 9.81a19.79 19.79 0 01-3.07-8.68A2 2 0 012 .91h3a2 2 0 012 1.72c.127.96.361 1.903.7 2.81a2 2 0 01-.45 2.11L6.09 8.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0122 16.92z"/>
                             </svg>
-                            361-944-8671
+                            361-944-8771
                         </a>
                         <br />
                         <button
